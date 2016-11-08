9fc76a11aefcc6f0f5181efaa7c53ce0d0d27b4d
emesc <tahder77@gmail.com>
2016-11-08 15:51:21 +1100

    Setup email functionality for sign up.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://try-photo-app.herokuapp.com/' }
+  # mailer
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'try-photo-app.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 end
