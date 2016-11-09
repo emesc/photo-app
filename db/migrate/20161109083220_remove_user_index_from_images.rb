6b50563e895ecabed0d1934d113ca10453ed6c62
emesc <tahder77@gmail.com>
2016-11-09 20:06:48 +1100

    Removed wrong index from schema-2.

--- db/migrate/20161109083220_remove_user_index_from_images.rb
+++ db/migrate/20161109083220_remove_user_index_from_images.rb
@@ -1,9 +1,5 @@
 class RemoveUserIndexFromImages < ActiveRecord::Migration[5.0]
-  def up
-    remove_index :users, name: 'index_images_on_user'
-  end
-
-  def down
-    add_index :users, name: 'index_images_on_user'
+  def change
+    remove_index :images, name: 'index_images_on_user'
   end
 end
